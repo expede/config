9c80c284b60505ea5d82e1059ff97bbf2dd650dc
Brooklyn Zelenka <be.zelenka@gmail.com>
2015-10-24 09:29:17 -0700

    Include.spacemacs and personal layer

---
+++ expede/packages.el
@@ -0,0 +1,51 @@
+;;; packages.el --- expede Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2015 Brooklyn Zelenka
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; List of all packages to install and/or initialize. Built-in packages
+;; which require an initialization must be listed explicitly in the list.
+(setq expede-packages
+    '(
+      crosshairs
+      rspec-mode
+      w3m
+      yard-mode
+      ))
+
+;; List of packages to exclude.
+(setq expede-excluded-packages '())
+
+;; For each package, define a function expede/init-<package-name>
+;;
+;; (defun expede/init-my-package ()
+;;   "Initialize my package"
+;;   )
+;;
+(defun expede/init-crosshairs ()
+  "Initialize my package"
+  (use-package crosshairs))
+
+(defun expede/init-rspec-mode ()
+  "Initialize my package"
+  (use-package rspec-mode))
+
+(defun expede/init-w3m ()
+  "Initialize my package"
+  (use-package w3m))
+
+(defun expede/init-yard-mode ()
+  "Initialize my package"
+  (use-package yard-mode))
+
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
